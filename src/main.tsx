f464b12282b5bfed5a89a8cb46edb7630e01e414
SandroMaglione <lass.maglio@gmail.com>
2024-07-27 09:59:52 +0200

    `router` setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import UseStatePage from "./use-state/page";
+
+const router = createBrowserRouter([
+  {
+    path: "/use-state",
+    element: <UseStatePage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
