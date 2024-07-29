f033b5278d37e82a9105d4914b5c24237cd6d7b3
SandroMaglione <lass.maglio@gmail.com>
2024-07-29 15:55:11 +0200

    xstate `fromCallback` interval

--- src/timer/machine/machine.ts
+++ src/timer/machine/machine.ts
@@ -1,27 +1,54 @@
-import { assign, setup } from "xstate";
+import { assign, fromCallback, sendTo, setup } from "xstate";
+
+type TickTimeEvent = Readonly<{ type: "tick-time" }>;
+type PauseEvent = Readonly<{ type: "pause" }>;
+
+type CallbackEvent = PauseEvent;
 
 export const machine = setup({
   types: {
-    events: {} as
-      | Readonly<{ type: "pause" }>
-      | Readonly<{ type: "restart" }>
-      | Readonly<{ type: "tick-time" }>,
+    events: {} as Readonly<{ type: "restart" }> | TickTimeEvent | PauseEvent,
     context: {} as {
       time: number;
+      interval: number | null;
     },
   },
   guards: {
     isCompleted: ({ context }) => context.time <= 0,
   },
+  actors: {
+    interval: fromCallback<CallbackEvent>(({ sendBack, receive }) => {
+      const interval = setInterval(() => {
+        sendBack({ type: "tick-time" } satisfies TickTimeEvent);
+      }, 1000);
+
+      const clear = () => {
+        clearInterval(interval);
+      };
+
+      receive((event) => {
+        if (event.type) {
+          clear();
+        }
+      });
+
+      return clear;
+    }),
+  },
   actions: {
     onTickTime: assign(({ context }) => ({ time: context.time - 1 })),
+    onPauseTimer: sendTo("interval", { type: "pause" } satisfies CallbackEvent),
   },
 }).createMachine({
   id: "use-state-machine",
-  context: { time: 10 },
-  initial: "Idle",
+  context: { time: 10, interval: null },
+  initial: "Running",
   states: {
     Running: {
+      invoke: {
+        id: "interval",
+        src: "interval",
+      },
       always: {
         target: "Completed",
         guard: "isCompleted",
@@ -33,9 +60,11 @@ export const machine = setup({
     },
     Paused: {
       on: {
-        pause: { target: "Running" },
+        restart: { target: "Running" },
       },
     },
-    Completed: {},
+    Completed: {
+      type: "final",
+    },
   },
 });
--- src/timer/machine/page.tsx
+++ src/timer/machine/page.tsx
@@ -1,22 +1,14 @@
 import { useMachine } from "@xstate/react";
-import { useEffect } from "react";
 import { machine } from "./machine";
 
 export default function Page() {
   const [snapshot, send] = useMachine(machine);
-
-  useEffect(() => {
-    const interval = setInterval(() => send({ type: "tick-time" }), 1000);
-    return () => {
-      clearInterval(interval);
-    };
-  }, [send]);
-
   return (
     <div>
       <p>Time left: {snapshot.context.time} seconds</p>
-      {snapshot.matches("Completed") && <p>Done!</p>}
-      {snapshot.matches("Paused") ? (
+      {snapshot.matches("Completed") ? (
+        <p>Done!</p>
+      ) : snapshot.matches("Paused") ? (
         <button onClick={() => send({ type: "restart" })}>Restart</button>
       ) : (
         <button onClick={() => send({ type: "pause" })}>Pause</button>
